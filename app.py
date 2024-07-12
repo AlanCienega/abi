2f8424d0b32b5ca748d9c5c621f5847e1c1ecf2a
AlanCienega <craulalan115@gmail.com>
2024-07-11 23:59:29 -0600

    feat(Interpreter): Implementación de ciclos simples

--- app.py
+++ app.py
@@ -4,20 +4,20 @@ from parser import Parser
 from interpreter import Interpreter
 
 def main(filename):
-    with open(filename, 'r') as file:
-        code = file.read()
+	with open(filename, 'r') as file:
+		code = file.read()
 
-    lexer = Lexer(code)
-    tokens = lexer.tokenize()
+	lexer = Lexer(code)
+	tokens = lexer.tokenize()
 
-    parser = Parser(tokens)
-    ast = parser.parse()
+	parser = Parser(tokens)
+	ast = parser.parse()
 
-    interpreter = Interpreter(ast)
-    interpreter.interpret()
+	interpreter = Interpreter(ast)
+	interpreter.interpret()
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Uso: python app.py archivo.abi")
-    else:
-        main(sys.argv[1])
+	if len(sys.argv) != 2:
+		print("Uso: python app.py archivo.abi")
+	else:
+		main(sys.argv[1])

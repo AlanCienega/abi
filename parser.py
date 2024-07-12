8478e75fa406db20adace8b0b5f3996a6e6a1365
AlanCienega <craulalan115@gmail.com>
2024-07-11 23:28:10 -0600

    feat(Parser): Nodo de repetición

--- parser.py
+++ parser.py
@@ -16,6 +16,14 @@ class PrintNode(ASTNode):
     def __repr__(self):
         return f"PrintNode(value={self.value})"
 
+class RepeatNode(ASTNode):
+    def __init__(self, statement, times):
+        self.statement = statement
+        self.times = times
+
+    def __repr__(self):
+        return f"RepeatNode(statement={self.statement}, times={self.times})"
+
 class Parser:
     def __init__(self, tokens):
         self.tokens = tokens
@@ -36,6 +44,8 @@ class Parser:
             return self.parse_assignment()
         elif token[0] == 'PRINT':
             return self.parse_print()
+        elif token[0] == 'REPEAT':
+            return self.parse_repeat()
         elif token[0] in ['NEWLINE', 'COMMENT']:
             self.pos += 1
             return None
@@ -60,7 +70,14 @@ class Parser:
             self.pos += 1
             return token
         else:
-            raise SyntaxError(f"Expected NUMBER or STRING but got {token}")
+            raise SyntaxError(f"Expected a value but got {token}")
+
+    def parse_repeat(self):
+        self.consume('REPEAT')
+        statement = self.parse_statement()
+        times = self.consume('NUMBER')
+        self.consume('TIMES')
+        return RepeatNode(statement, int(times[1]))
 
     def consume(self, token_type):
         token = self.tokens[self.pos]
